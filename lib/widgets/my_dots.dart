4fc7e7c3c4ff179c12c29febcec1d9cf932d4faf
Unknown <ueder.mds.cardoso@gmail.com>
2020-04-27 01:23:03 -0300

    save 2

--- lib/widgets/my_dots.dart
+++ lib/widgets/my_dots.dart
@@ -5,8 +5,9 @@ import 'package:flutter/material.dart';
 class MyDots extends StatelessWidget {
 
   final int currentIndex;
+  final double top;
 
-  const MyDots({Key key, this.currentIndex}) : super(key: key);
+  const MyDots({Key key, this.currentIndex, this.top}) : super(key: key);
 
   Color getColor(int index){
     return index == currentIndex ? Colors.white : Colors.white38; //Se é a mesma página
@@ -14,38 +15,41 @@ class MyDots extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        AnimatedContainer(
-          duration: Duration(milliseconds: 300),
-          height: 7,
-          width: 7,
-          decoration: BoxDecoration(
-              color: getColor(0),
-              shape: BoxShape.circle
+    return Positioned(
+      top: top,
+      child: Row(
+        children: <Widget>[
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            height: 7,
+            width: 7,
+            decoration: BoxDecoration(
+                color: getColor(0),
+                shape: BoxShape.circle
+            ),
           ),
-        ),
-        SizedBox(width: 8), //Espaçamento
-        AnimatedContainer(
-          duration: Duration(milliseconds: 300),
-          height: 7,
-          width: 7,
-          decoration: BoxDecoration(
-              color: getColor(1),
-              shape: BoxShape.circle
+          SizedBox(width: 8), //Espaçamento
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            height: 7,
+            width: 7,
+            decoration: BoxDecoration(
+                color: getColor(1),
+                shape: BoxShape.circle
+            ),
           ),
-        ),
-        SizedBox(width: 8), //Espaçamento
-        AnimatedContainer(
-          duration: Duration(milliseconds: 300),
-          height: 7,
-          width: 7,
-          decoration: BoxDecoration(
-              color: getColor(2),
-              shape: BoxShape.circle
-          ),
-        )
-      ],
+          SizedBox(width: 8), //Espaçamento
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            height: 7,
+            width: 7,
+            decoration: BoxDecoration(
+                color: getColor(2),
+                shape: BoxShape.circle
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/widgets/page_view_app.dart
+++ lib/widgets/page_view_app.dart
@@ -5,26 +5,33 @@ import 'package:nubank_flutter/widgets/card_app.dart';
 class PageViewApp extends StatelessWidget {
   final double top;
   final ValueChanged<int> onChanged;
+  final GestureDragUpdateCallback onPanUpdate; //Fazendo o nosso próprio callback
+  final bool showMenu;
 
-  const PageViewApp({Key key, this.top, this.onChanged}) : super(key: key);
+  const PageViewApp({Key key, this.top, this.onChanged, this.onPanUpdate, this.showMenu}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
 
-    return Positioned(
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 300),
+        curve: Curves.easeOut,
         top: top,
         height: MediaQuery.of(context).size.height * 0.45,
         left: 0,
         right: 0,
         //width: MediaQuery.of(context).size.width,
-        child: PageView(
-          onPageChanged: onChanged,
-              physics: BouncingScrollPhysics(),
-              children: <Widget>[
-                CardApp(),
-                CardApp(),
-                CardApp()
-              ]
+        child: GestureDetector(
+          onPanUpdate: onPanUpdate, //Move o item quando é clicado
+          child: PageView(
+            onPageChanged: onChanged,
+                physics: showMenu ? NeverScrollableScrollPhysics() : BouncingScrollPhysics(), //Não mover quando a condição for verdadeira
+                children: <Widget>[
+                  CardApp(),
+                  CardApp(),
+                  CardApp()
+                ]
+          ),
         ),
     );
   }
